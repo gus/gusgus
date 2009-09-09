10ca0a1c67375bb73e4c7b113e406522f8bdf61b
Justin Knowlden <gus@gusg.us>
2009-09-08 19:40:19 -0500

    Quick add of title to sketches

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,7 +2,7 @@ function jsonFlickrFeed(data) {
   var list = $$(".flickr").first();
   $(data.items).each(function(item) {
     var image_url = item.media.m.replace(/_m\.jpg$/, "_s.jpg");
-    var image = new Element("img", {src: image_url});
+    var image = new Element("img", {src: image_url, alt: item.title, title: item.title});
     var link = new Element("a", {href: item.link}).update(image);
     var list_item = new Element("li").update(link);
     list.insert({bottom: list_item});
