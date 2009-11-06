7edae7d83bd2d89d8744362436aff91e5a232856
Justin Knowlden <gus@gusg.us>
2009-11-06 08:35:30 -0600

    Pissed off, but sketches are loading again

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,14 +1,15 @@
 function jsonFlickrFeed(data) {
-  var list = $$(".flickr").first();
-  $(data.items).each(function(item) {
+  var list = $(".flickr");
+  $(data.items).each(function(i) {
+    var item = this;
     var image_url = item.media.m.replace(/_m\.jpg$/, "_s.jpg");
-    var image = new Element("img", {src: image_url, alt: item.title, title: item.title});
-    var link = new Element("a", {href: item.link}).update(image);
-    var list_item = new Element("li").update(link);
-    list.insert({bottom: list_item});
+    var image = $("<img>").attr({src: image_url, alt: item.title, title: item.title});
+    var link = $("<a>").attr({href: item.link}).append(image);
+    var list_item = $("<li>").append(link);
+    list.append(list_item);
   });
 };
 
-jQuery(document).ready(function() {
-  jQuery.getScript($$(".flickr").first().readAttribute("data-url"));
+$(document).ready(function() {
+  $.getScript($(".flickr").attr("data-url"));
 });
