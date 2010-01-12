1fe8e498520e2f9ebce06d20de454f1f809fa05b
Justin Knowlden <gus@gusg.us>
2010-01-11 18:31:54 -0600

    Basic layout of new site is there

--- _site/javascripts/application.js
+++ _site/javascripts/application.js
@@ -5,7 +5,7 @@ function jsonFlickrFeed(data) {
     var image_url = item.media.m.replace(/_m\.jpg$/, "_s.jpg");
     var image = $("<img>").attr({src: image_url, alt: item.title, title: item.title});
     var link = $("<a>").attr({href: item.link}).append(image);
-    var list_item = $("<li class='grid_1'>").append(link);
+    var list_item = $("<li>").append(link);
     list.append(list_item);
   });
 };
